fefb75ccf889848c8accaf12f770965c73f0cc01
Luke Bakken <luke@bakken.io>
2023-07-28 08:24:09 -0700

    Print URI

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ httpClient.DefaultRequestHeaders.Add("Authorization", $"Basic {basicAuth}");
 
 string jsonBody = "{\"payload\":\"hello world from sage 100\",\"payload_encoding\":\"string\",\"properties\":{},\"routing_key\":\"CustomerOrder_SageSync\"}";
 StringContent stringContent = new StringContent(jsonBody, System.Text.Encoding.UTF8, "application/json");
-HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(new Uri("http://localhost:15672/api/exchanges/%2f/amq.default/publish"), stringContent);
+Uri uri = new Uri("http://localhost:15672/api/exchanges/%2f/amq.default/publish");
+Console.WriteLine($"URI: {uri.ToString()}");
+HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(uri, stringContent);
 
 Console.WriteLine(httpResponseMessage);
